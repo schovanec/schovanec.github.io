6e58479894fa50d34a13983d6ce6f5a620d43c90
Steve Chovanec <schovanec@fastmail.fm>
2015-01-21 21:24:17 -0500

    add google analytics

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,9 @@
+---
+# front matter is included so that this file will be processed by Jekyll
+---
 $(function ($) {
 
+	// add captions to photos
 	$('.post-content > img[alt], .post-content :not(figure) > img[alt]')
 		.wrap('<figure class="post-img">')
 		.each(function () {
@@ -10,3 +14,14 @@ $(function ($) {
 		});
 
 });
+
+// google analytics
+(function () {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', '{{ site.google_analytics_id }}', 'auto');
+	ga('send', 'pageview');
+})();
