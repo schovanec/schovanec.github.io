80bc562f8e40a5a78d173fb0b0fe15e9570d392c
Steve Chovanec <git@stevechovanec.com>
2022-04-23 01:33:27 -0400

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,49 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 
 module.exports = {
-  entry: [
-    path.resolve("src", "js", "app.js"),
-    path.resolve("src", "styles", "app.css"),
-  ],
+  devtool: "source-map",
+  entry: {
+    app: [
+      path.resolve("src", "js", "app.js"),
+      path.resolve("src", "styles", "app.scss"),
+    ],
+  },
   output: {
     path: path.resolve("static", "assets"),
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
+    publicPath: "/assets/",
+    clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/i,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new WebpackManifestPlugin({
+      fileName: path.resolve(__dirname, "data/manifest.json"),
+    }),
+  ],
 };
